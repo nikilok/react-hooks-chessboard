b29e1540d3c5d7b08ad16acec82caf85c456c9ae
Nikil K <nikilok@gmail.com>
2019-05-05 11:44:52 +0200

    Changed the rejoin message from 6 mins -> 1hour. Dads request. Have already made backend changes to incorporate this

--- src/common/appConstant.js
+++ src/common/appConstant.js
@@ -6,9 +6,9 @@ const GAME_LEAVE_MSG =
     "Your client disconnected, and will go to the lobby. Please Rejoin the game then.",
   PLAYER_DISCONNECT_TITLE = "Player Disconnect",
   GAME_TERMINATE_MSG =
-    "The game is ending, as all players did not rejoin back before 6 minutes.",
+    "The game is ending, as all players did not rejoin back before 1 hour.",
   PLAYER_DISCONNECT_MESSAGE =
-    "The other player has disconnected. You could wait for him to reconnect before 6 minutes, or the game will auto terminate.",
+    "The other player has disconnected. You could wait for him to reconnect before 1 hour, or the game will auto terminate.",
   PLAYER_RECONNECTED_TITLE = "Player Reconnected",
   PLAYER_RECONNECTED_MESSAGE =
     "The other player is back, the game will not be cancelled.";
